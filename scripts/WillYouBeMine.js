55ce0b0be99df5be7a3ab26e8ca8d460f2fa1b86
Alicia AyersLott <amayers92@gmail.com>
2021-08-06 10:22:59 -0500

    playing with CSS

--- scripts/WillYouBeMine.js
+++ scripts/WillYouBeMine.js
@@ -2,19 +2,20 @@ import { displaySelectedGovernor, displayColonyAvailableResources } from "./Colo
 import { GovernorsSelection } from "./Governors.js"
 import { facilityList } from "./Facilities.js"
 
+
 export const beMineHTML = () => {
     return `
         <h1>Will you be Mine</h1>
 
         <article id="facilities">
         <h2>Active Mining Facilities</h2>
-            <section>
+            <section class="facility_list">
             ${facilityList()}
             </section>
 
         </article>
 
-        <article>Chosen minerals and purchase button go in here
+        <article id="minerals">Chosen minerals and purchase button go in here
             <h2>Chosen Minerals</h2>
             <section>Chosen minerals stuffs go in here</section>
             <button>BuTtOn</button>
