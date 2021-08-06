7f45c73e76435ed097b6b95718766083b3ed3431
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-06 09:34:44 -0500

    camelCasing on findGovernor function

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,7 +1,7 @@
 import { getGovernors, getColonies, getChosenMinerals} from "./database.js"
 
 
- const findgovernor = (id) =>{
+ const findGovernor = (id) =>{
     const governors = getGovernors();
     
     const foundGovernor = governors.find((gov) =>  gov.id === id)
@@ -9,7 +9,7 @@ import { getGovernors, getColonies, getChosenMinerals} from "./database.js"
 }
 
  const findColony = () =>{
-    const governor = findgovernor(getChosenMinerals().governorId)
+    const governor = findGovernor(getChosenMinerals().governorId)
     const colonies = getColonies();
     if (governor !== undefined){
         const foundColony = colonies.find((colony) => colony.id === governor.colonyId)
@@ -18,11 +18,11 @@ import { getGovernors, getColonies, getChosenMinerals} from "./database.js"
     }
 }
 export const displaySelectedGovernor = () =>{
-    const governor = findgovernor(getChosenMinerals().governorId)
+    const governor = findGovernor(getChosenMinerals().governorId)
     return (governor === undefined ?  "" : `<h2>${governor.name}</h2>`)
 }
 export const displayColonyAvailableResources = () =>{
-    const governor = findgovernor(getChosenMinerals().governorId)
+    const governor = findGovernor(getChosenMinerals().governorId)
     const colony = findColony(governor)
     if (colony === undefined){
         return "<h2>Colonies</h2>"
